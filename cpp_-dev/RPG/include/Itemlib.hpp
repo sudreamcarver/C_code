6bfaa0deac2a4583e33e753556b4b812b272576a
carver <lt72496@outlook.com>
2025-12-12 03:28:55 -1000

    .

---
+++ cpp_-dev/RPG/include/Itemlib.hpp
@@ -0,0 +1,21 @@
+#include <Item.hpp>
+#include <map>
+#include <string>
+
+struct ItemBluePrint
+{
+    std::string name;
+    OwnerType type;
+    std::string description;
+    Effct effct;
+    int value;
+};
+
+class ItemLibrary
+{
+  private:
+    std::map<std::string, ItemBluePrint> bluePrint;
+
+  public:
+    ItemLibrary () {}
+};
